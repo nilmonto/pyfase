001eb87276fbc770d206e5c901338de84a26645b
Joaci Morais <joaci.morais@gmail.com>
2016-03-14 21:30:26 -0400

    initial release

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyfase',
+    version='1.0.0',
+    packages=[''],
+    url='https://jomorais@bitbucket.org/jomorais/pyfase.git',
+    license='GNU LESSER GENERAL PUBLIC LICENSE - Version 2.1, February 1999',
+    author='Joaci Morais',
+    author_email='joaci.morais@gmail.com',
+    description='A Fast-Asynchronous-microService-Environment based on ZeroMQ.'
+)
