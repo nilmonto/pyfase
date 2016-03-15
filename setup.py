92a93526af1d5bf4f01ad745c50b0679ad7762d2
Joaci Morais <joaci.morais@gmail.com>
2016-03-14 22:26:03 -0400

    setup.py: add install_requires

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='pyfase',
     version='1.0.0',
     packages=[''],
     url='https://github.com/jomorais/pyfase',
-    license='GNU LESSER GENERAL PUBLIC LICENSE - Version 2.1, February 1999',
+    license='GPLv3',
     author='Joaci Morais',
     author_email='joaci.morais@gmail.com',
     description='A Fast-Asynchronous-microService-Environment based on ZeroMQ.',
